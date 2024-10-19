21f655f84d79dae77425ea5331eb11cb181e5f1f
Hugo Oliveira <143663456+Hugo-oliveira10@users.noreply.github.com>
2024-10-19 17:30:19 -0300

    Add files via upload
    atualização da rotas de acesso das paginas
    dia 18/10 aguardando mais instrunções

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FuncionarioController;
 
+// Rota para a página inicial
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); 
+});
+
+// routes/web.php
+Route::get('/opcoes', function () {
+    return view('opcoes');
+});
+
+//routes/admin.php
+Route::get('/admin', function (){
+    return view('admin');
+});
+
+//routes/funcionarios.php
+Route::get('/funcionarios', function(){
+    return view('funcionarios');
+});
+
+//routes/cadastro.php
+Route::get('/create', function(){
+    return view('create');
 });
