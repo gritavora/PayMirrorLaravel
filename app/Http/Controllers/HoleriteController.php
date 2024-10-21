94fdd9032d7260ce02e9bd4068213140528bc736
Hugo Oliveira <143663456+Hugo-oliveira10@users.noreply.github.com>
2024-10-21 02:11:43 -0300

    Add files via upload
    Pagina de Holerite adicionada

---
+++ app/Http/Controllers/HoleriteController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HoleriteController extends Controller
+{
+    public function mostrarHolerite()
+    {
+        // Exemplo de dados do trabalhador
+        $trabalhador = (object) [
+            'nome' => 'João Silva',
+            'cargo' => 'Desenvolvedor'
+        ];
+
+        $mes = 'Outubro';
+        $salarioBruto = 5000.00; // Exemplo de salário bruto
+        $inss = 550.00; // Exemplo de desconto do INSS
+        $irrf = 250.00; // Exemplo de desconto do IRRF
+
+        // Retornar a view com os dados
+        return view('holerite', compact('trabalhador', 'mes', 'salarioBruto', 'inss', 'irrf'));
+    }
+}
