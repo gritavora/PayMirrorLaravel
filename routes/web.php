75f05fb57f54e724e97876206b82fe8bad2ec538
Hugo Oliveira <143663456+Hugo-oliveira10@users.noreply.github.com>
2024-10-23 17:46:39 -0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -6,44 +6,29 @@ use App\Http\Controllers\FuncionarioController;
 use App\Http\Controllers\HoleriteController;
 use App\Http\Controllers\AvisoController;
 
-
 // Rota para a página inicial
 Route::get('/', function () {
     return view('welcome'); 
 });
 
-// routes/web.php
+// Rota para opções
 Route::get('/opcoes', function () {
     return view('opcoes');
 });
 
-//routes/admin.php
-Route::get('/admin', function (){
+// Rota para admin
+Route::get('/admin', function () {
     return view('admin');
 });
 
-//routes/funcionarios.php
-Route::get('/funcionarios', function(){
-    $nome ="Hugo";//essa variavel tem de acessar o banco para mostrar o nome do usuario na hora de entrar
-    return view('funcionarios',['nome'=> $nome]);// retorno da variavem dentro de um array de arrow function
-});
-
-
-//routes/cadastro.php
-Route::get('/create', function(){
-    return view('create');
-});
+// Rota para a lista de funcionários
+Route::resource('funcionarios', FuncionarioController::class);
 
 // Rota Holerite
 Route::get('/funcionarios/holerite', [HoleriteController::class, 'mostrarHolerite']);
 
-//controller do aviso 
 // Rota para exibir a página de avisos
 Route::get('/avisos', [AvisoController::class, 'index'])->name('avisos.index');
 
 // Rota para processar o envio de novos avisos
 Route::post('/avisos', [AvisoController::class, 'store'])->name('avisos.store');
-
-
-
-
